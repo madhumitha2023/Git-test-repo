767e73921cfc8a24fc25183d155ca1f5176558dd
madhumitha2023 <madhumitha.srivatsa@gmail.com>
2024-06-19 19:08:20 -0400

    conflicts creation

--- src/MainFeature.java
+++ src/MainFeature.java
@@ -4,18 +4,18 @@ public class MainFeature {
     public static void main(String[] args) {
         System.out.println("Aarthi");
         System.out.println("Aarthi");
-        System.out.println("Creating Conflict");
+        System.out.println("Testing Conflict");
         System.out.println("Testing Conflict");
         System.out.println("Aarthi");
+        System.out.println("Testing");
+        System.out.println("Testing Conflict");
+        System.out.println("Testing Conflict");
+        System.out.println("Madhumitha");
+        System.out.println("Madhumitha");
+        System.out.println("Testing Conflict");
+        System.out.println("Testing Conflict");
+        System.out.println("Madhumitha");
+        System.out.println("Madhumitha");
         System.out.println("Aarthi");
-        System.out.println("Creating Conflict");
-        System.out.println("Testing Conflict");
-        System.out.println("Madhumitha");
-        System.out.println("Madhumitha");
-        System.out.println("Creating Conflict");
-        System.out.println("Testing Conflict");
-        System.out.println("Madhumitha");
-        System.out.println("Madhumitha");
-        System.out.println("Madhumitha");
     }
 }
