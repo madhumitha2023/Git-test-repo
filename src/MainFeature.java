bca055158bf9a9b352f052b148d3f8ce622acc9c
Aarthis98 <aarthini.sivasamy@outlook.com>
2024-06-19 19:03:44 -0400

    adding conflict

--- src/MainFeature.java
+++ src/MainFeature.java
@@ -4,18 +4,22 @@ public class MainFeature {
     public static void main(String[] args) {
         System.out.println("Aarthi");
         System.out.println("Aarthi");
+        System.out.println("Test File");
         System.out.println("Creating Conflict");
         System.out.println("Testing Conflict");
         System.out.println("Aarthi");
-        System.out.println("Aarthi");
+        System.out.println("Test File");
+        System.out.println("Creating Conflict");
+        System.out.println("Test File");
+        System.out.println("Madhumitha");
+        System.out.println("Madhumitha");
+        System.out.println("Test File");
         System.out.println("Creating Conflict");
         System.out.println("Testing Conflict");
         System.out.println("Madhumitha");
         System.out.println("Madhumitha");
-        System.out.println("Creating Conflict");
-        System.out.println("Testing Conflict");
         System.out.println("Madhumitha");
-        System.out.println("Madhumitha");
-        System.out.println("Madhumitha");
+        System.out.println("Test File");
+        System.out.println("Test File");
     }
 }
